f8502ef0cd78328622bc81929911b178c3e19bfb
Khalil JARRAYA <kjarraya@idf.canalce.com>
2018-12-14 11:36:39 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+        // ILogger<DataContext> _logger;
+
+        // public DataContext(DbContextOptions<DataContext> options, ILogger<DataContext> logger)
+        //   : base(options)
+        // {
+        //   _logger = logger;
+        // }
+
+    }
+}
