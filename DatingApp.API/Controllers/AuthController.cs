6badc55d8fab3d528c04d6a05155d97c33ba8c78
Khalil JARRAYA <kjarraya@idf.canalce.com>
2018-12-21 10:34:47 +0100

    End of section 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -54,7 +54,7 @@ namespace DatingApp.API.Controllers
 
             var claims = new[]{
                 new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
-                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Username)
+                new Claim(ClaimTypes.Name,userFromRepo.Username)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
