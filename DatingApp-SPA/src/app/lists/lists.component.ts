388528926d6682513dfc25b9e38c40cc337edb93
Khalil JARRAYA <kjarraya@idf.canalce.com>
2018-12-21 12:11:39 +0100

    End od section 7

---
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './lists.component.html',
+  styleUrls: ['./lists.component.less']
+})
+export class ListsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
